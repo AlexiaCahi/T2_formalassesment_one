bbf2a894a48a3052490434bc419a40efe82bb294
AlexiaCahi <231245@virtualwindow.co.za>
2023-05-28 19:03:00 +0200

    Commit 1

---
+++ js/index.js
@@ -0,0 +1,42 @@
+
+
+// HTML elements
+
+addStudent = () => {
+    let first = document.getElementById("first").value
+    let email = document.getElementById("email").value
+    let number = document.getElementById("number").value
+    let password = document.getElementById("password").value
+    let subject = document.getElementById("subject").value
+}
+
+
+// Event listener for form submission
+form.addform ("submit", function(event) {
+  event.preventDefault(); // Prevent form from submitting and page refresh
+  
+  // Get input values
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const number = numberInput.value;
+  
+  // Validate input
+  if (name === '' || email === '' || number === '') {
+    alert('Please fill in all fields');
+    return;
+  }
+  
+  // Create student object
+  const student = {
+    name: name,
+    email: email,
+    number: number
+  };
+  
+  // Do something with the student data
+  console.log(student);
+  
+  // Clear form
+  form.reset();
+});
+
